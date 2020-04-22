8e5358fc4899599ca48a2fb0da0f82f9fa2935fb
shinnosuke.uesaka <shinnosuke.uesaka.sub@gmail.com>
2020-04-22 03:48:28 +0000

    post

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
     has_secure_password
-    # the orders matter?
+    
+    has_many :microposts
+    # 順番関係あるか？
 end
